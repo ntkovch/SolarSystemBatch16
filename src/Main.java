311c7c09e59612960d09099e6b1694a59ed781e3
ntkovch <ntkovch@gmail.com>
2023-07-08 20:15:04 +0300

    The feature Sun has been added sucessfully

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,9 @@ public class Main {
         Omnax.Sun="1";
         Omnax.Planets="2";
         Omnax.Star="4";
+        FeatureSun XEo=new FeatureSun();
+        XEo.Name="treo";
+        XEo.size="32432km";
+        XEo.temp="34444885 celcius";
     }
 }
